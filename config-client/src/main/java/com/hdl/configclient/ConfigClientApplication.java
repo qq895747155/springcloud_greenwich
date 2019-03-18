e6b79a21d9e8eb383e68ffc57076db22a8965677
hdl <handl_bj@163.com>
2019-03-19 02:26:09 +0800

    spring cloud config配置

--- config-client/src/main/java/com/hdl/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/hdl/configclient/ConfigClientApplication.java
@@ -1,29 +1,28 @@
-package com.hdl.up_server;
+package com.hdl.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-//开启服务注册和发现功能，将本实例注册到注册中心
 @EnableDiscoveryClient
-//此服务为其它服务提供基础服务
 @RestController
-public class UpServerApplication {
+public class ConfigClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UpServerApplication.class, args);
+        SpringApplication.run(ConfigClientApplication.class, args);
     }
 
-    @Value("${server.port}")
-    String port;
+    @Value("${name}")
+    String name;
 
-    @RequestMapping("/action")
-    public String action(){
-        return "hello,I is base server and my port is "+ port;
+    @RequestMapping("/name")
+    public String name(){
+        return "name="+name;
     }
 
 }
---
+++ config-server/src/main/java/com/hdl/configserver/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.hdl.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+//开启配置中心服务
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
